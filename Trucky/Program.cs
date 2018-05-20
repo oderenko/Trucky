e6c117498e7cdbbfbfcd657503066b2526d8cedc
Oleksandr Derenko <astra_97@ukr.net>
2018-05-20 17:33:43 +0300

    - added connection string - configured main and Startup

--- Trucky/Program.cs
+++ Trucky/Program.cs
@@ -1,25 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Trucky.Models.DB;
 
-namespace Trucky
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
+namespace Trucky {
+  public class Program {
+    public static void Main(string[] args) {
+      var host = BuildWebHost(args);
+
+      using (var scope = host.Services.CreateScope()) {
+        var services = scope.ServiceProvider;
+        try {
+          var context = services.GetRequiredService<TruckyContext>();
         }
+        catch (Exception ex) {
+          var logger = services.GetRequiredService<ILogger<Program>>();
+          logger.LogError(ex, "An error occurred while seeding the database.");
+        }
+      }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+      host.Run();
     }
+
+    public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
+  }
 }
--- Trucky/Startup.cs
+++ Trucky/Startup.cs
@@ -1,50 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO.Compression;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using Trucky.Models.DB;
 
-namespace Trucky
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+namespace Trucky {
+  public class Startup {
+    public IConfiguration Configuration { get; }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+    public Startup(IConfiguration configuration) {
+      Configuration = configuration;
     }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services) {
+      services
+        .AddMvcCore()
+        .AddJsonFormatters(settings => {
+          settings.DateFormatString = "MM/dd/yyyy";
+        })
+        .AddAuthorization()
+        .AddDataAnnotations();
+
+      // Configure response compression
+      services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Fastest);
+      services.AddResponseCompression(options => { options.Providers.Add<GzipCompressionProvider>(); });
+
+      services.AddDbContext<TruckyContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+      app.UseStaticFiles();
+
+      ConfigureRoutes(app, env);
+    }
+
+    private void ConfigureRoutes(IApplicationBuilder app, IHostingEnvironment env) {
+      if (env.IsDevelopment()) {
+        app.UseBrowserLink();
+        app.UseDeveloperExceptionPage();
+      }
+      else {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseMvc(routes => {
+        routes.MapRoute(
+            name: "default",
+            template: "{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+  }
 }
